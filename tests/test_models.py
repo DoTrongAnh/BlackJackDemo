2c7902774eede6aab806f23c04c7ea49bb1839ad
Do Trong Anh <anh.trong.do.1994@gmail.com>
2025-03-09 11:38:08 -0600

    Add mock test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,4 @@
+from blackjack_demo import models
 from blackjack_demo.models import Card, CardSuit, CardValue, Deck, Player
 import pytest
 from random import choice
@@ -24,3 +25,29 @@ def test_deck_inventory():
     expected_deck_size = len([v for v in CardValue]) * len([s for s in CardSuit])
     actual_deck = set(new_deck.cards)
     assert len(actual_deck) == expected_deck_size, f'Deck should have {expected_deck_size} unique cards, not {len(actual_deck)}'
+
+
+def test_deck_empty():
+    new_deck = Deck()
+    new_deck.cards = []
+    assert new_deck.is_empty(), 'Deck should be empty'
+
+
+@pytest.mark.parametrize(
+    'win_score,hand,is_bust,is_blackjack',
+    (21, [CardValue.ACE, CardValue.TEN], False, True),
+    (21, [CardValue.TEN, CardValue.ACE], False, True),
+    (21, [CardValue.ACE, CardValue.JACK], False, True),
+    (21, [CardValue.JACK, CardValue.ACE], False, True),
+    (21, [CardValue.ACE, CardValue.QUEEN], False, True),
+    (21, [CardValue.QUEEN, CardValue.ACE], False, True),
+    (21, [CardValue.ACE, CardValue.KING], False, True),
+    (21, [CardValue.KING, CardValue.ACE], False, True),
+)
+def test_player_score(mocker, win_score, hand, is_bust, is_blackjack):
+    new_player = Player()
+    mocker.patch('models.BLACKJACK_WIN', return_value=win_score)
+    for card_value in hand:
+        new_player.add_card(Card(card_value, choice([s for s in CardSuit])))
+    assert new_player.is_bust() == is_bust, f'Player busting should be {is_bust}'
+    assert new_player.is_blackjack() == is_blackjack, f'Player blackjack should be {is_blackjack}'
